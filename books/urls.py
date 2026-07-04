adcddda32aa0c954841d39037e6df2f1d2b15bc2
Paul Miller <szcezliwy@hotmail.com>
2026-07-04 19:21:59 -0400

    feat: add book update view, form error handling, and edit link

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,6 @@
-from .views import book_list, book_create
+from .views import book_list, book_create, book_update
 from django.urls import path
 
 urlpatterns = [path('', book_list, name='book_list'),
-               path('create/', book_create, name='book_create'),]
+               path('create/', book_create, name='book_create'),
+               path('<int:book_id>/edit', book_update, name='book_update'),]
--- books/views.py
+++ books/views.py
@@ -6,12 +6,28 @@ def book_list(request):
     books = Book.objects.all()
     return render(request, 'books/book_list.html', {'books': books})
 
+# book creation form - if form valid save to db, if not, render page with existing info and error message
 def book_create(request):
+    is_update = False
     if request.method == 'POST':
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')
+        return render(request, 'books/book_form.html', {'form': form, 'is_update': is_update})
     else:
         form = BookForm()
-        return render(request,'books/book_form.html', {'form': form})
+        return render(request,'books/book_form.html',{'form': form, 'is_update':is_update})
+
+def book_update(request, book_id):
+    is_update = True
+    book = Book.objects.get(id = book_id)
+    if request.method == 'POST':
+       form = BookForm(request.POST, instance=book)
+       if form.is_valid():
+           form.save()
+           return redirect('book_list')
+       return render(request, 'books/book_form.html', {'form': form, 'is_update': is_update})
+    else:
+        form = BookForm(instance=book)
+        return render(request, 'books/book_form.html', {'form': form, 'is_update':is_update})
