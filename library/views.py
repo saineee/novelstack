c8cb255890900330e3f87c2c2d6294fbc6de73aa
Paul Miller <szcezliwy@hotmail.com>
2026-07-06 21:13:56 -0400

    feat: add UserBook delete view with confirmation page

--- library/views.py
+++ library/views.py
@@ -27,3 +27,11 @@ def userbook_update(request, userbook_id):
     else:
         form = UserBookForm(instance = book)
         return render(request, 'library/update.html', {'form': form, "userbook_id": userbook_id})
+
+def userbook_delete(request, userbook_id):
+    book = get_object_or_404(UserBook, id=userbook_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('library')
+    else:
+        return render(request, 'library/delete.html', {'userbook': book, 'userbook_id': userbook_id})
