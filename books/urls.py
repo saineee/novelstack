146461b1e682217162d019d43072e2a2a1dee8bf
Paul Miller <szcezliwy@hotmail.com>
2026-07-07 23:59:09 -0400

    feat: add search/filter to book list via book_list view

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,7 @@
-from .views import book_list, book_create, book_update, book_delete, book_details
+from .views import book_create, book_update, book_delete, book_details, sorted_book_list
 from django.urls import path
 
-urlpatterns = [path('', book_list, name='book_list'),
+urlpatterns = [path('', sorted_book_list, name='sorted_book_list'),
                path('create/', book_create, name='book_create'),
                path('<int:book_id>/edit', book_update, name='book_update'),
                path('<int:book_id>/delete', book_delete, name='book_delete'),
--- books/views.py
+++ books/views.py
@@ -3,10 +3,6 @@ from library.models import UserBook
 from .models import Book
 from .forms import BookForm
 
-def book_list(request):
-    books = Book.objects.all()
-    return render(request, 'books/book_list.html', {'books': books})
-
 # book creation form - if form valid save to db, if not, render page with existing info and error message
 def book_create(request):
     is_update = False
@@ -14,7 +10,7 @@ def book_create(request):
         form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('book_list')
+            return redirect('books/book_list.html')
         return render(request, 'books/book_form.html', {'form': form, 'is_update': is_update})
     else:
         form = BookForm()
@@ -27,7 +23,7 @@ def book_update(request, book_id):
        form = BookForm(request.POST, instance=book)
        if form.is_valid():
            form.save()
-           return redirect('book_list')
+           return redirect('books/book_list.html')
        return render(request, 'books/book_form.html', {'form': form, 'is_update': is_update})
     else:
         form = BookForm(instance=book)
@@ -37,7 +33,7 @@ def book_delete(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
        book.delete()
-       return redirect('book_list')
+       return redirect('books/book_list.html')
     else:
         return render(request, 'books/book_delete.html', {'book': book})
 
@@ -45,3 +41,17 @@ def book_details(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     already_exists = UserBook.objects.filter(book=book, user=request.user).exists()
     return render(request, 'books/book_details.html', {'book': book, 'already_exists': already_exists})
+
+def sorted_book_list(request):
+    sorted_books = Book.objects.order_by('title')
+    title = request.GET.get('title')
+    genre = request.GET.get('genre')
+    status = request.GET.get('status')
+    if title:
+        sorted_books = sorted_books.filter(title__icontains=title)
+    if genre:
+        sorted_books = sorted_books.filter(genre__icontains=genre)
+    if status:
+        sorted_books = sorted_books.filter(status = status)
+    return render(request, 'books/book_list.html', {'books': sorted_books})
+
