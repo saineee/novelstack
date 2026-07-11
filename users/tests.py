09084fbd93f4f672367435ab0a1587a44a6dcb80
Paul Miller <szcezliwy@hotmail.com>
2026-07-11 00:37:37 -0400

    test: add dupe email rejection test for CreateUserForm

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+import pytest
+from .forms import CreateUserForm
 
-# Create your tests here.
+
+
+def test_duplicate_email_rejected(db):
+    User = get_user_model()
+    User.objects.create_user(username='testuser123', email='testuser@gmail.com', password='testuser123')
+    form = CreateUserForm(data={
+        'username': 'testuser123',
+        'email': 'testuser@gmail.com',
+        'password1': 'testuser123',
+        'password2': 'testuser123',
+    })
+    assert form.is_valid() is False
+    assert 'email' in form.errors
