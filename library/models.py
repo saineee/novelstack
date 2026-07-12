4f3b6f807af46dc27faf440798b94584e48ec90e
Paul Miller <szcezliwy@hotmail.com>
2026-07-12 00:23:57 -0400

    feat: add check constraint for date_ended after date_started

--- library/models.py
+++ library/models.py
@@ -1,3 +1,7 @@
+from django.db.models.query_utils import Q
+
+from django.db.models.expressions import F
+
 from django.db import models
 from django.conf import settings
 from books.models import Book
@@ -13,12 +17,16 @@ class UserBook(models.Model):
     ]
     class Meta:
         constraints = [
-            models.UniqueConstraint(fields=['user', 'book'], name='unique_user_book')
+            models.UniqueConstraint(fields=['user', 'book'], name='unique_user_book'),
+            models.CheckConstraint(
+                condition=Q(date_ended__gte=F('date_started')),
+                name = "date_ended_after_date_started"
+            )
         ]
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     current_chapter = models.IntegerField(null=True, blank=True)
-    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='to read')
+    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='to_read')
     rating = models.IntegerField(null=True, blank=True)
     date_started = models.DateField(null=True, blank=True)
     date_ended = models.DateField(null=True, blank=True)
