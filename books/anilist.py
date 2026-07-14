9ad2ea95ed1a90d169594812236139a56ccc1f33
Paul Miller <szcezliwy@hotmail.com>
2026-07-14 11:44:57 -0400

    feat: add AniList fetch and extract candidate funcs

--- books/anilist.py
+++ books/anilist.py
@@ -1,3 +1,11 @@
+import logging
+from collections import defaultdict
+
+import requests
+
+logger = logging.getLogger("novelstack")
+
+
 query = """
 query ($search: String) {
     Page (perPage: 10) {
@@ -16,6 +24,30 @@ query ($search: String) {
 }   
 """
 
-variables = {"search": "Lord of the Mysteries"}
-
 url = "https://graphql.anilist.co"
+
+def fetch_candidates(title):
+    variables = {"search": title}
+    logger.debug("searching AniList for %s", title)
+    response = requests.post(url, json={'query': query, 'variables': variables})
+    return response.json()
+
+
+
+def extract_candidates(response):
+    cleaned_data = []
+    candidates = response['data']['Page']['media']
+    for candidate in candidates:
+        title_data = candidate['title']
+        eng_title = title_data.get('english')
+        rom_title = title_data.get('romaji')
+
+        cleaned = {
+            'id': candidate.get('id', None),
+            'anilist_cover_url': candidate.get('coverImage', {}).get('large', None),
+            'format': candidate.get('format', None),
+            'title': eng_title if eng_title else rom_title
+        }
+
+        cleaned_data.append(cleaned)
+    return cleaned_data
