088b6977435793b03a9c9977eb0f4a94148cdaae
Paul Miller <szcezliwy@hotmail.com>
2026-07-18 13:00:32 -0400

    feat: add fetch_id and shared MEDIA_FIELDS for book refetch

--- books/anilist.py
+++ books/anilist.py
@@ -3,11 +3,7 @@ import requests
 
 logger = logging.getLogger("novelstack")
 
-
-query = """
-query ($search: String) {
-    Page (perPage: 10) {
-    media (search: $search, type: MANGA) {
+MEDIA_FIELDS = """
         id
         title { 
             romaji 
@@ -37,15 +33,36 @@ query ($search: String) {
                 }
             }
         }  
+        """
+
+SEARCH_QUERY = """
+query ($search: String) {
+    Page (perPage: 10) {
+    media (search: $search, type: MANGA) {
+ """ + MEDIA_FIELDS + """       
         }
     }
 }   
 """
 
+ID_QUERY = """
+query ($id: Int) {
+    Media (id: $id) {
+""" + MEDIA_FIELDS + """    
+    }
+}
+"""
 url = "https://graphql.anilist.co"
+
 
 def fetch_candidates(title):
     variables = {"search": title}
     logger.debug("searching AniList for %s", title)
-    response = requests.post(url, json={'query': query, 'variables': variables})
+    response = requests.post(url, json={'query': SEARCH_QUERY, 'variables': variables})
+    return response.json()
+
+def fetch_id(anilist_id):
+    variables = {"id": anilist_id}
+    logger.debug("searching AniList for %s", anilist_id)
+    response = requests.post(url, json={'query': ID_QUERY, 'variables': variables})
     return response.json()
