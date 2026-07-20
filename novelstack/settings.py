dc3c579f85e749863e1a2a0279fc1613cc95ff09
Paul Miller <szcezliwy@hotmail.com>
2026-07-20 16:10:02 -0400

    chore: read SECRET_KEY, DEBUG, ALLOWED_HOSTS from env

--- novelstack/settings.py
+++ novelstack/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cj(ke+5-4)x!c34sof2yc2l%ay6h%dr-fsyl)z^ta#q5n)p49n'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 # Application definition
 
